00b6357aaaf13fd88d768b27f57afd03266b0aac
Nayommi <nklisyt@gmail.com>
2021-06-01 17:51:25 +0300

    playground

---
+++ Contents.swift
@@ -0,0 +1,48 @@
+var table1 = 1
+for index in 1...10 {
+    print("\(table1)*\(index) = \(index * table1)")  
+}
+var table2 = 2
+for index in 1...10 {
+    print("\(table2)*\(index) = \(index * table2)")  
+}
+
+var table3 = 3
+for index in 1...10 {
+    print("\(table3)*\(index) = \(index * table3)")  
+}
+
+var table4 = 4
+for index in 1...10 {
+    print("\(table4)*\(index) = \(index * table4)")  
+}
+
+var table5 = 5
+for index in 1...10 {
+    print("\(table5)*\(index) = \(index * table5)")  
+}
+
+var table6 = 6
+for index in 1...10 {
+    print("\(table6)*\(index) = \(index * table6)")  
+}
+
+var table7 = 7
+for index in 1...10 {
+    print("\(table7)*\(index) = \(index * table7)")  
+}
+
+var table8 = 8
+for index in 1...10 {
+    print("\(table8)*\(index) = \(index * table8)")  
+}
+
+var table9 = 9
+for index in 1...10 {
+    print("\(table9)*\(index) = \(index * table9)")  
+}
+
+var table10 = 10
+for index in 1...10 {
+    print("\(table10)*\(index) = \(index * table10)")  
+}
